e8feead69936bb25ba01f1fb5eee3af0947249b4
Mikew1999 <mikewalters1234@gmail.com>
2021-01-15 20:12:56 +0000

    changed name of script.js to map.js, changed script urls for google maps API and initialised map

---
+++ assets/scripts/map.js
@@ -0,0 +1,9 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 5,
+        center: {
+            lat: 38.707008,
+            lng: -9.135640
+        }
+    });
+}
