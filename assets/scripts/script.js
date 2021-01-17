43773b31dbaf69b23415630e11e6138bbae6790e
Mikew1999 <mikewalters1234@gmail.com>
2021-01-17 18:49:19 +0000

    removed script.js

--- assets/scripts/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $("#navbar-toggler-icon").toggle(function() {
-        $("div.navbar-collapse").addClass("push-down");
-    }), function() {
-        $("div.navbar-collapse").removeClass("push-down");
-    }
-})
