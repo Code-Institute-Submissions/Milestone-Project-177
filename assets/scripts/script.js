ba397f35c0d58ed8afa70e0f5f8625bad2c3307a
Mikew1999 <mikewalters1234@gmail.com>
2021-02-03 11:00:30 +0000

    fixed problem with slideshow

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -8,24 +8,16 @@ function slides(n) {
     slideshow(Index += n);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-    
-//starts slideshow
-function slideshow(a) {
+function slideshow(n) {
     var i;
     var slides = document.getElementsByClassName("slide");
-
-    if (a > slides.length) { 
-        slideIndex = 1 
-    }
-    if (a < 1) {
-        Index = slides.length
-    }
+    if (n > slides.length) { Index = 1 }
+    if (n < 1) { Index = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    //shows current slide
+    if (slides.length == 0) {
+        slides[Index].style.display = "block";
+    }
     slides[Index - 1].style.display = "block";
 }
