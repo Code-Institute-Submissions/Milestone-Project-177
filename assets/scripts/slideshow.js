26febe042a667bf413461199eaa55783570ef56f
Mikew1999 <mikewalters1234@gmail.com>
2021-02-07 18:39:29 +0000

    fixed validation issue with slideshow.js, changed colour of destination search section and made responsive.

--- assets/scripts/slideshow.js
+++ assets/scripts/slideshow.js
@@ -11,8 +11,8 @@ function slides(n) {
 function slideshow(n) {
     var i;
     var slides = document.getElementsByClassName("slide");
-    if (n > slides.length) { Index = 1 }
-    if (n < 1) { Index = slides.length }
+    if (n > slides.length) { Index = 1; }
+    if (n < 1) { Index = slides.length; }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
@@ -20,6 +20,6 @@ function slideshow(n) {
         slides[Index].style.display = "block";
     }
     else {
-    slides[Index - 1].style.display = "block";
+        slides[Index - 1].style.display = "block";
     }
 }
