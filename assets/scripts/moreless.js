325ad46e2f400f8ad39f024009cb39a5ad83500e
Mikew1999 <mikewalters1234@gmail.com>
2021-02-15 08:59:58 +0000

    added jquery code to moreless.js

--- assets/scripts/moreless.js
+++ assets/scripts/moreless.js
@@ -1,16 +1,15 @@
-$(document).ready(function() {
-    //on click event handler
-    $(".moreLessButton").click(function() {
-        //toggles the visibility of the more and ellipsis classes on click
-        $(".more").toggle();
-        $(".ellipsis").toggle();
+$(document).ready(function () {
+    $(".moreLessButton").each(function(i){
+        $(this).click(function() {
+            $(".more").toggle();
+            $(".ellipsis").toggle();
 
-        //Changes text in read more / less button
-        if($(".moreLessButton").text() == "Read Less") {
-            $(".moreLessButton").text("Read More");
-        }
-        else {
-            $(".moreLessButton").text("Read Less");
-        }
+            if ($(".moreLessButton").text() == "Read More") {
+                $(".moreLessButton").text("Read Less");
+            }
+            else {
+                $(".moreLessButton").text("Read More");
+            }
+        })
     })
 })
