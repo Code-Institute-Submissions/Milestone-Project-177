784b5729d4d88e1de5446a8ef190fb7f748a52cf
Mikew1999 <mikewalters1234@gmail.com>
2021-02-17 12:10:01 +0000

    added accordion in hot deals section, added accordion.js with jquery code

--- assets/scripts/newsletter.js
+++ assets/scripts/newsletter.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
     $("#newsletter-button").click(function() {
         $("#newsletter-modal").css('display', 'block');
     })
+    $(".close-button").click(function() {
+        $("#newsletter-modal").css('display', 'none');
+    })
 })
+
--- assets/scripts/slideshow.js
+++ assets/scripts/slideshow.js
@@ -8,18 +8,24 @@ function slides(n) {
     slideshow(Index += n);
 }
 
+//slideshow function for popular locations section
 function slideshow(n) {
     var i;
+    //Gets slides
     var slides = document.getElementsByClassName("slide");
     if (n > slides.length) { Index = 1; }
     if (n < 1) { Index = slides.length; }
+    //hides slides which aren't active
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
+    //fixes issue when going from slide 3 to slide 1
     if (slides.length == 0) {
         slides[Index].style.display = "block";
     }
+    //sets current slide to display block
     else {
         slides[Index - 1].style.display = "block";
     }
 }
+
