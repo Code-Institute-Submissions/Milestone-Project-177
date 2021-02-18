7e30854167433411ada2014df8623b146ed5dc46
Mikew1999 <mikewalters1234@gmail.com>
2021-02-18 03:49:20 +0000

    added content to popular attractions section. added jquery script in filter.js to filter the popular attractions

---
+++ assets/scripts/filter.js
@@ -0,0 +1,14 @@
+function showAll() {
+    $(".usa-canada").css("display", "block");
+    $(".uk").css("display", "block");
+}
+
+function showUk() {
+    $(".usa-canada").css("display", "none");
+    $(".uk").css("display", "block");
+}
+
+function showUsaCanada() {
+    $(".usa-canada").css("display", "block");
+    $(".uk").css("display", "none");
+}
